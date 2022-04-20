9f29a214ec3e774b41ca3f03f3205100ea7d1c04
vothanhdanh <999conan999@gmail.com>
2022-04-20 19:29:23 +0700

    v0.0

---
+++ src/App.js
@@ -0,0 +1,193 @@
+import React, { Component } from 'react';
+import {get_star,get_icon,get_random_css} from './lib/fs';
+import Sliderz from './components/slider';
+const items=[
+     {
+       img_url:"https://anbinhnew.com/wp-content/uploads/2021/01/giuong-sat-gia-re-mau-xanh.jpg",
+       title:{
+         id:"GA01",
+         price_from:1300000,
+         price_to:0,
+         message:''
+       },
+     },
+     {
+       img_url:"https://anbinhnew.com/wp-content/uploads/2021/01/Giuong-sat-hop-cao-cap-don-gian.jpg",
+       title:{
+         id:"GB01",
+         price_from:0,
+         price_to:0,
+         message:'Giá tùy theo kích thước'
+       },
+     },
+     {
+       img_url:"https://anbinhnew.com/wp-content/uploads/2021/01/giuong-sat-hoang-gia-hcm-noi-that-an-binh-1-720x720.jpg",
+       title:{
+         id:"HG01",
+         price_from:0,
+         price_to:3500000,
+         message:'Miễn phí vận chuyển'
+       },
+     },
+     {
+       img_url:"https://anbinhnew.com/wp-content/uploads/2021/01/Giuong-sat-don-1m2-mau-ngang-2.jpg",
+       title:{
+         id:"GC01",
+         price_from:2300000,
+         price_to:3500000,
+         message:'Giá tùy theo kích thước'
+       },
+     },
+   ];
+Number.prototype.format = function(n, x, s, c) {
+var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
+     num = this.toFixed(Math.max(0, ~~n));
+
+return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
+};
+ 
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+  }
+  render() {
+    return (
+      <React.Fragment> xxx
+               <div className='row mg-0'>
+                    <div className='col-12 col-xl-1 col-xxl-2'>1</div>
+                    <div className='col-12 col-sm-12 col-md-7 col-lg-8 col-xl-8 col-xxl-6'>
+                         <div className='row'>
+                              <div className='col-12 col-sm-6 col-md-12 col-lg-6'>
+                                   <div className='wrap-card card-1' style={{backgroundColor: '#1598496b'}}>
+                                        <div className="tieu-de">
+                                             <h2 className='title-1'>
+                                                  Giường sắt giá rẻ 8 tấc, 80cm, 1m, 1m2, 1m4, 1m6 1m8 x dài 2m 
+                                             </h2>
+                                        </div>
+                                        <div className="danh-gia">
+                                             <span>Đánh giá  :</span>
+                                             {get_star(2.5)}
+                                             <span class={`ribbon ${get_random_css()}`}>Mẫu số 1</span>
+                                        </div>
+                                        <Sliderz items={items}/>
+                                        <div className='xem-sp'>
+                                             <p>
+                                                  <a className="button1" href='tel:0963226771' target="_blank">
+                                                       <span>
+                                                       {get_icon('phone','25px')}
+                                                       Liên hệ
+                                                       </span>
+                                                  </a>
+                                             </p>
+                                        </div>
+                                       <div>
+                                             <h3 style={{fontSize: '19px',marginTop: '15px'}}>
+                                                 * Thông tin sản phẩm
+                                             </h3>
+                                             <div className='table-x'>
+                                                  <table className='table-z1' border='1'>
+                                                       <tbody>
+                                                            <tr className='tr-z1'>
+                                                                 <td className='td-z1 f-2'>
+                                                                     <strong> Giá Giường sắt giá rẻ</strong>
+                                                                 </td>
+                                                                 <td className='td-z2 f-8'>
+                                                                 + Sắt hộp ( chịu lực nặng 600kg) + Sơn tĩnh điện (chống rỉ sét).
+                                                                 </td>
+                                                            </tr>
+                                                            <tr className='tr-z1'>
+                                                                 <td className='td-z1 f-2'>
+                                                                     <strong> Chất liệu</strong>
+                                                                 </td>
+                                                                 <td className='td-z2 f-8'>
+                                                                  + Sắt hộp ( chịu lực nặng 600kg) + Sơn tĩnh điện (chống rỉ sét).
+                                                                 </td>
+                                                            </tr>
+                                                            <tr className='tr-z1'>
+                                                                 <td className='td-z1 f-2'>
+                                                                     <strong> Kích thước hỗ trợ</strong>
+                                                                 </td>
+                                                                 <td className='td-z2 f-8'>
+                                                                 🔔🔔🔔 + Sắt hộp ( chịu lực nặng 600kg) + Sơn tĩnh điện (chống rỉ sét).
+                                                                 </td>
+                                                            </tr>
+                                                       </tbody>
+                                                  </table>
+                                             </div>
+                                             <div>
+                                                  <span className='thong-tin'>Thông tin về chúng tôi 👉 <a href="#thong-tin">Nội thất An bình</a></span>
+                                             </div>
+                                       </div>
+                                       <div>
+                                             <h3 style={{fontSize: '19px',marginTop: '15px'}}>
+                                                 * Thông tin sản phẩm
+                                             </h3>
+                                             <div className='table-x'>
+                                                  <table className='table-z1' border='1'>
+                                                       <tbody>
+                                                            <tr className='tr-z1'>
+                                                                 <td className='td-z1 f-2'>
+                                                                     <strong> Giá</strong>
+                                                                 </td>
+                                                                 <td className='td-z2 f-7'>
+                                                                 + Sắt hộp ( chịu lực nặng 600kg) + Sơn tĩnh điện (chống rỉ sét).
+                                                                 </td>
+                                                                 <td className='td-z2 f-1'>
+                                                                  <button class="btn btn-danger">Mua</button>
+                                                                 </td>
+                                                            </tr>
+                                                            <tr className='tr-z1'>
+                                                                 <td className='td-z1 f-2'>
+                                                                     <strong> Chất liệu</strong>
+                                                                 </td>
+                                                                 <td className='td-z2 f-7'>
+                                                                  + Sắt hộp ( chịu lực nặng 600kg) + Sơn tĩnh điện (chống rỉ sét).
+                                                                 </td>
+                                                                 <td className='td-z2 f-1'>
+                                                                  <button  class="btn btn-danger">Mua</button>
+                                                                 </td>
+                                                            </tr>
+                                                            <tr className='tr-z1'>
+                                                                 <td className='td-z1 f-2'>
+                                                                     <strong> Kích thước hỗ trợ</strong>
+                                                                 </td>
+                                                                 <td className='td-z2 f-7'>
+                                                                 🔔🔔🔔 + Sắt hộp ( chịu lực nặng 600kg) + Sơn tĩnh điện (chống rỉ sét).
+                                                                 </td>
+                                                                 <td className='td-z2 f-1'>
+                                                                  <button class="btn btn-danger">Mua</button>
+                                                                 </td>
+                                                            </tr>
+                                                       </tbody>
+                                                  </table>
+                                             </div>
+                                       </div>
+                                   </div>
+                              </div>
+                              <div className='col-12 col-sm-6 col-md-12 col-lg-6'>
+                                   <div className='wrap-card card-1' style={{backgroundColor: '#f50c0c59'}}>
+                                        <div className="tieu-de">
+                                             <h2 className='title-1'>
+                                                  Giường sắt giá rẻ 8 tấc, 80cm, 1m, 1m2, 1m4, 1m6 1m8 x dài 2m 
+                                             </h2>
+                                        </div>
+                                        <div className="danh-gia">
+                                             <span>Đánh giá :</span>
+                                             {get_star(4)}
+                                             <span class={`ribbon ${get_random_css()}`}>Mẫu số 1</span>
+                                        </div>
+                                        <Sliderz  items={items}/>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+                    <div id="thong-tin" className='col-12 col-sm-12 col-md-5 col-lg-4 col-xl-3 col-xxl-4'>3</div>
+               </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/slider.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import Slider from "react-slick";
+class Sliderz extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      nav1: null,
+      nav2: null
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      nav1: this.slider1,
+      nav2: this.slider2
+    })
+  }
+  //
+  show_img_large=(items)=>{
+    if(items!=undefined){
+      let rs=[];
+      items.forEach((e,i) => {
+        let title=e.title;let show_ma_sp='';
+        //
+        let show_mess='';
+        if(title!=undefined&&title.id!=undefined){
+          show_ma_sp=`Mã sản phẩm: ${title.id}`;
+          if(title.price_from==0&&title.price_to==0&&title.message==""){
+          }else{
+            show_mess=<div className='messz'>
+              {title.price_from>0&&title.price_to>0&&<p style={{textAlign:'center',fontSize:'14px',color:'#fdfdfd',marginBottom:'0px'}}>Giá từ <span style={{fontWeight:'600',color:'#ede614',}}>{title.price_from.format(0, 3, '.', ',')} đ</span> đến <span style={{fontWeight:'600',color:'#ede614',}}>{title.price_to.format(0, 3, '.', ',')} đ</span></p>}
+              {title.price_from>0&&title.price_to==0&&<p style={{textAlign:'center',fontSize:'14px',color:'#fdfdfd',marginBottom:'0px'}}>Giá <span style={{fontWeight:'600',color:'#ede614',}}>{title.price_from.format(0, 3, '.', ',')} đ</span></p>}
+              {title.price_from==0&&title.price_to>0&&<p style={{textAlign:'center',fontSize:'14px',color:'#fdfdfd',marginBottom:'0px'}}>Giá <span style={{fontWeight:'600',color:'#ede614',}}>{title.price_to.format(0, 3, '.', ',')} đ</span></p>}
+              {title.message!=''&&<p style={{textAlign:'center',color:'#fdfdfd',fontSize: '12px',marginBottom:'0px',height:'21px'}}>*{title.message}</p>}
+            </div>
+          }
+        }
+        //
+        rs.push(
+          <div className='handle'>
+            <a>
+              <img src={e.img_url} width={'100%'}/>
+            </a>
+            {show_mess}
+            <div className='ma-sp'>{show_ma_sp}</div>
+          </div>
+        )
+      });
+      return rs;
+    }
+  }
+  //
+  show_img_small=(items)=>{
+    if(items!=undefined){
+      let rs=[];
+      items.forEach((e,i) => {
+        rs.push(
+          <div className='handle-1'>
+            <img src={e.img_url} width={'63px'}/>
+          </div>
+        )
+      });
+      return rs;
+    }
+  }
+  //
+  render() {
+    let img_list=this.props.items;//[todo]
+    let slidesToShow=5;
+    if(img_list!=undefined){
+      if(img_list.length<5) slidesToShow=img_list.length; 
+    }
+
+    return (
+      <React.Fragment>  
+          <div className='slider-z'>
+          <Slider
+            asNavFor={this.state.nav2}
+            ref={slider => (this.slider1 = slider)}
+            arrows={false}
+            infinite={false}
+          >
+            {this.show_img_large(img_list)}
+        </Slider>
+        {/*  */}
+        <div className='wrap-img-small'>
+          <Slider
+            asNavFor={this.state.nav1}
+            ref={slider => (this.slider2 = slider)}
+            slidesToShow={slidesToShow}
+            swipeToSlide={true}
+            focusOnSelect={true}
+            variableWidth={true}
+            infinite={false}
+          >
+            {this.show_img_small(img_list)}
+          </Slider>
+        </div>
+          </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Sliderz;
